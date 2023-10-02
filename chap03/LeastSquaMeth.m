2994237b4f80c9cdfc377c16bd49bb62da435541
ltylover <1192588363@qq.com>
2023-10-02 15:44:22 +0800

    改进最小二乘法曲线拟合并绘图输出

--- chap03/LeastSquaMeth.m
+++ chap03/LeastSquaMeth.m
@@ -1,14 +1,17 @@
 clc;
 clear;
 
+% 此处仅为多项式最小二乘法曲线拟合
+
 dataTable = readtable('LeastSquaMeth.xlsx');
 dataArray = table2array(dataTable);
 LeastSquaTable = dataArray(:, :);
 [m, n] = size(LeastSquaTable);
 
-% 拟合函数为：a+b*x^2
+% 拟合函数为：y=a+b*x^2
 % 2个未知数，故维度为2
-dimen = 4;
+dimen = 2;
+% Amat*Cmat=Bmat, Cmat是由a和b系数组成的列向量
 Amat = zeros(dimen);
 Bmat = zeros(dimen, 1);
 
@@ -16,29 +19,29 @@ for i = 1:dimen
     for j = 1:dimen
         ySum = 0;
         for k = 1:n
-
+            % i为维数，f为张成子空间中各多项式函数的次数
             switch i
                 case 1
                     f = 0;
                 case 2
                     f = 1;
-                case 3
-                    f = 2;
-                case 4
-                    f = 3;
+%                 case 3
+%                     f = 2;
+%                 case 4
+%                     f = 3;
             end
-
+            % j为维数，g表示为张成子空间中各多项式函数的次数
             switch j
                 case 1
                     g = 0;
                 case 2
                     g = 1;
-                case 3
-                    g = 2;
-                case 4
-                    g = 3;
+%                 case 3
+%                     g = 2;
+%                 case 4
+%                     g = 3;
             end
-%             y = LeastSquaTable(1, i)^f*LeastSquaTable(1,k)^g;
+
             y = LeastSquaTable(1,k)^(f+g);
             ySum = ySum + y;
 
@@ -55,10 +58,10 @@ for i = 1:dimen
                 f = 0;
             case 2
                 f = 1;
-            case 3
-                f = 2;
-            case 4
-                f = 3;
+%             case 3
+%                 f = 2;
+%             case 4
+%                 f = 3;
         end
         y = LeastSquaTable(1, k)^f*LeastSquaTable(2, k);
         ySum = ySum + y;
@@ -67,10 +70,24 @@ for i = 1:dimen
 end
 
 Cmat = Amat^(-1)*Bmat;
-% A = [1.178097245, 0, 1.570796327;
-%     0,  1.570796327, 0;
-%      1.570796327, 0, 3.141592654];
-% B = [2;
-%      2;
-%      2];
-% C = A^(-1)*B
+
+hold on;
+PotX = LeastSquaTable(1, :);
+PotY = LeastSquaTable(2, :);
+scatter(PotX, PotY)
+
+a = Cmat(1, 1);
+b = Cmat(2, 1);
+InterA = LeastSquaTable(1, 1);
+InterB = LeastSquaTable(1, n);
+i = 1;
+iStep = 0.001; % 步长
+for x = InterA:iStep:InterB
+    yFun(1, i) = a + b*x;% 拟合的函数
+    i = i + 1;
+end
+x = [InterA:iStep:InterB];
+plot(x, yFun);
+hold off;
+
+

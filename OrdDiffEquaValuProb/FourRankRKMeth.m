74f74cdd405543b134db255ecd72150e64d09676
ltylover <1192588363@qq.com>
2023-10-27 15:42:35 +0800

    修改常微分方程初值问题的文件夹名称，增加4阶RK方法

---
+++ OrdDiffEquaValuProb/FourRankRKMeth.m
@@ -0,0 +1,26 @@
+clc;
+clear;
+
+% y' = x + y
+
+h = 0.2;
+a = 0;
+b = 1;
+xArray = 0;
+yArray = 1;
+k = 2;
+for i = a+h:h:b
+    x_n = xArray(1, k-1);
+    y_n = yArray(1, k-1);
+    k1 = xyFun(x_n, y_n);
+    k2 = xyFun(x_n + 0.5*h, y_n + 0.5*h*k1);
+    k3 = xyFun(x_n + 0.5*h, y_n + 0.5*h*k2);
+    k4 = xyFun(x_n + h, y_n + h*k3);
+    yArray(1, k) = y_n + 1/6*h*(k1 + 2*k2 + 2*k3 + k4);
+    xArray(1, k) = i;
+    k = k + 1;
+end
+
+hold on
+plot(xArray, yArray);
+hold off
